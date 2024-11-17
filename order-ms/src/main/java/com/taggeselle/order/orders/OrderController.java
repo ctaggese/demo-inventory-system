115be70ead0ee651f890cf3c97808a6b3bd963e5
Chris Taggeselle <christaggeselle@gmail.com>
2024-11-17 23:50:59 +0100

    Update ui, disable cors

--- order-ms/src/main/java/com/taggeselle/order/orders/OrderController.java
+++ order-ms/src/main/java/com/taggeselle/order/orders/OrderController.java
@@ -9,7 +9,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/orders")
+@RequestMapping( path = "api/orders", produces = "application/json")
 public class OrderController {
 
     @Autowired
--- order-ui/src/app/app.config.ts
+++ order-ui/src/app/app.config.ts
@@ -4,12 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { ApiModule } from './api/api.module';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
 
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
     provideHttpClient(),
-    importProvidersFrom([  ApiModule.forRoot({ rootUrl: 'http://localhost:8080' }),])
+    importProvidersFrom([  ApiModule.forRoot({ rootUrl: environment.apiUrl }),])
   ]
 };
